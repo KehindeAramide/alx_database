9e069e458cc7b5d253accd1313cd588275c338d6
KehindeAramide <kenned2k4@yahoo.com>
2023-09-14 14:18:02 +0100

    TASK4 UPD

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,7 +1,3 @@
--- CREATE A TABLE first_table IN DATABASE MYSQL;
-
-CREATE TABLE IF NOT EXISTS `first_table`
-(
-    `id` INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `name` VARCHAR(256) NOT NULL,
-);
+-- Creates a table called first_table with values id and name in my MySQL server.
+CREATE TABLE IF NOT EXISTS `first_table` 
+(`id` INT, `name` VARCHAR(256));
