fb9186d12c04f65b4cd4b0ec0ae2389963f37f77
KehindeAramide <kenned2k4@yahoo.com>
2023-09-15 13:39:11 +0100

    task6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- Create or modify the database hbtn_0d_usa
+-- Creates database hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Create table states
-CREATE TABLE IF NOT EXISTS states;
+-- Creates table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (
     id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256)
