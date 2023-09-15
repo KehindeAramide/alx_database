8e3ebce9cb44fcaea26f90b129ebeddfcb6b3634
KehindeAramide <kenned2k4@yahoo.com>
2023-09-15 13:59:54 +0100

    task8 upd

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,5 @@
 -- Lists all cities of Califonia that can be found in database hbtn_0d_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Creates table states
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
-(
-    name California, id INT AUTO_DECREMENT(cities_id)
-);
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT states.id FROM states WHERE states.name = 'California')
+ORDER BY cities.id ASC;
