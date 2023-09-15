8a93a3e6a77af113c020a1be5f417cb1b4401c8d
KehindeAramide <kenned2k4@yahoo.com>
2023-09-15 13:56:15 +0100

    task8

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- Lists all cities of Califonia that can be found in database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Creates table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
+(
+    name California, id INT AUTO_DECREMENT(cities_id)
+);
